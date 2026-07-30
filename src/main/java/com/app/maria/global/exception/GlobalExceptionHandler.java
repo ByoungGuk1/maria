fba679b78b555c3672ea02a13832a9557d662cba
ByoungGuk1 <bksong121212@gmail.com>
2026-07-30 19:26:30 +0900

    feat : Exception 추가로 인한 GlobalExceptionHandler 수정

--- src/main/java/com/app/maria/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/maria/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.app.maria.global.exception;
 
 import com.app.maria.domain.account.exception.AccountException;
+import com.app.maria.domain.account.exception.AccountNotFoundException;
+import com.app.maria.domain.account.exception.DuplicateAccountException;
+import com.app.maria.domain.account.exception.InvalidAccountRequestException;
 import com.app.maria.domain.member.exception.MemberException;
 import com.app.maria.domain.member.exception.MemberNotFoundException;
 import com.app.maria.global.response.ApiResponseDTO;
@@ -23,6 +26,18 @@ public class GlobalExceptionHandler {
   //Account
   @ExceptionHandler(AccountException.class)
   public ResponseEntity<ApiResponseDTO<Void>>handleAccountException(AccountException e) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponseDTO.of(e.getMessage()));
+  }
+  @ExceptionHandler(AccountNotFoundException.class)
+  public ResponseEntity<ApiResponseDTO<Void>>handleAccountNotFoundException(AccountException e) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponseDTO.of(e.getMessage()));
+  }
+  @ExceptionHandler(DuplicateAccountException.class)
+  public ResponseEntity<ApiResponseDTO<Void>>handleDuplicateAccountException(AccountException e) {
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(ApiResponseDTO.of(e.getMessage()));
+  }
+  @ExceptionHandler(InvalidAccountRequestException.class)
+  public ResponseEntity<ApiResponseDTO<Void>>handleInvalidAccountRequestException(AccountException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponseDTO.of(e.getMessage()));
   }
 }
