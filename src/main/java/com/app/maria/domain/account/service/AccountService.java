85ed6644338c4ef3cd3dad2cdab1b9a8dcf06b82
ByoungGuk1 <bksong121212@gmail.com>
2026-07-30 19:27:56 +0900

    feat : 계좌 단일 조회 기능 및 계좌 변경 로그 조회 기능 추가
    기타 다른 서비스 로직을 일관성 있는 반환 타입으로 변경 후 각각의 예외처리로 변경

--- src/main/java/com/app/maria/domain/account/service/AccountService.java
+++ src/main/java/com/app/maria/domain/account/service/AccountService.java
@@ -1,13 +1,17 @@
 package com.app.maria.domain.account.service;
 
-import com.app.maria.domain.account.dto.AccountDTO;
+import com.app.maria.domain.account.dto.request.AccountRequestDTO;
+import com.app.maria.domain.account.dto.response.AccountLogResponseDTO;
+import com.app.maria.domain.account.dto.response.AccountResponseDTO;
 
 import java.util.List;
 
 public interface AccountService {
-  List<AccountDTO> findAll();
-  AccountDTO applyAccount(AccountDTO accountDTO);
-  AccountDTO openAccount(AccountDTO accountDTO);
-  AccountDTO rejectAccount(AccountDTO accountDTO);
-  AccountDTO reapplyAccount(AccountDTO accountDTO);
+  List<AccountResponseDTO> findAll();
+  AccountResponseDTO applyAccount(AccountRequestDTO accountRequestDTO);
+  AccountResponseDTO openAccount(Long accountId);
+  AccountResponseDTO rejectAccount(Long accountId);
+  AccountResponseDTO reapplyAccount(Long accountId, AccountRequestDTO accountRequestDTO);
+  AccountResponseDTO getAccount(Long accountId);
+  List<AccountLogResponseDTO> getStatusLogList(Long accountId);
 }
