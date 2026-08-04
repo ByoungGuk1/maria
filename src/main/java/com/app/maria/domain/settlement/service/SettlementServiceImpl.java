834b7673bd82467053724f1c7fc70fe48a0170df
ByoungGuk1 <bksong121212@gmail.com>
2026-08-04 13:47:57 +0900

    refector : service 검증 부분 메서드 분리

--- src/main/java/com/app/maria/domain/settlement/service/SettlementServiceImpl.java
+++ src/main/java/com/app/maria/domain/settlement/service/SettlementServiceImpl.java
@@ -4,10 +4,7 @@ import com.app.maria.domain.settlement.dto.KrwExchangeDTO;
 import com.app.maria.domain.settlement.dto.SettlementBatchDTO;
 import com.app.maria.domain.settlement.dto.SettlementItemDTO;
 import com.app.maria.domain.settlement.dto.SettlementJoinDTO;
-import com.app.maria.domain.settlement.exception.InvalidSettlementException;
-import com.app.maria.domain.settlement.exception.KrwExchangeNotFoundException;
-import com.app.maria.domain.settlement.exception.SettlementBatchNotFoundException;
-import com.app.maria.domain.settlement.exception.SettlementItemNotFoundException;
+import com.app.maria.domain.settlement.exception.*;
 import com.app.maria.domain.settlement.mapper.KrwExchangeMapper;
 import com.app.maria.domain.settlement.mapper.SettlementBatchMapper;
 import com.app.maria.domain.settlement.mapper.SettlementItemMapper;
@@ -40,9 +37,7 @@ public class SettlementServiceImpl implements SettlementService {
   @Override
   @Transactional(readOnly = true)
   public SettlementBatchDTO getSettlementBatch(Long batchId) {
-    if(batchId == null || batchId <= 0){
-      throw new InvalidSettlementException("batchId - 요청 값 오류");
-    }
+    validateLongPositive(batchId,"batchId");
     return settlementBatchMapper.selectBatchById(batchId).orElseThrow(()->new SettlementBatchNotFoundException("batch id로 배치 조회 실패"));
   }
 
@@ -73,9 +68,7 @@ public class SettlementServiceImpl implements SettlementService {
   @Override
   @Transactional(readOnly = true)
   public SettlementJoinDTO getSettlementItem(Long batchId, Long itemId) {
-    if (itemId == null || itemId <= 0) {
-      throw new InvalidSettlementException("itemId - 요청 값 오류");
-    }
+    validateLongPositive(itemId, "itemId");
     getSettlementBatch(batchId);
     SettlementJoinDTO cursor = SettlementJoinDTO.builder()
         .batchId(batchId)
@@ -87,9 +80,16 @@ public class SettlementServiceImpl implements SettlementService {
   @Override
   @Transactional(readOnly = true)
   public KrwExchangeDTO getKrwExchange(Long exchangeId) {
-    if(exchangeId == null || exchangeId <= 0){
-      throw new InvalidSettlementException("exchangeId - 요청 값 오류");
-    }
+    validateLongPositive(exchangeId, "exchangeId");
     return krwExchangeMapper.selectExchangeById(exchangeId).orElseThrow(()->new KrwExchangeNotFoundException("환전 조회 실패"));
+  }
+
+  private void validateLongPositive(Long value, String fieldName) {
+    if (value == null) {
+      throw new InvalidSettlementException(fieldName + " - 요청 값 오류");
+    }
+    if (value.compareTo(0L) <= 0) {
+      throw new InvalidSettlementException(fieldName + "은 0보다 커야 합니다.");
+    }
   }
 }
