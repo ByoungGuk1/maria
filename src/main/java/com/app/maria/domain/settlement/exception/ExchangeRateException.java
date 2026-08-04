7f807bef504e84727ac37be9617bf4e5d5da9697
ByoungGuk1 <bksong121212@gmail.com>
2026-08-04 09:31:07 +0900

    feat : 배치(확정산) 관련 exception 파일 추가

---
+++ src/main/java/com/app/maria/domain/settlement/exception/ExchangeRateException.java
@@ -0,0 +1,7 @@
+package com.app.maria.domain.settlement.exception;
+
+public class ExchangeRateException extends SettlementException {
+  public ExchangeRateException(String message) {
+    super(message);
+  }
+}
