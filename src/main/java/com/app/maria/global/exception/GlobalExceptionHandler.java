1509848239b8cfa4704823bf8d049f48f585967e
iingjeong <silvmin42@gmail.com>
2026-08-04 11:07:34 +0900

    feat: 전역 예외 처리 핸들러 추가

--- src/main/java/com/app/maria/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/maria/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.app.maria.global.exception;
 
+import com.app.maria.domain.inbound.exception.InboundException;
+import com.app.maria.domain.inbound.exception.InboundNotFoundException;
 import com.app.maria.domain.member.exception.MemberException;
 import com.app.maria.domain.member.exception.MemberNotFoundException;
 import com.app.maria.global.response.ApiResponseDTO;
@@ -16,6 +18,15 @@ public class GlobalExceptionHandler {
   }
   @ExceptionHandler(MemberNotFoundException.class)
   public ResponseEntity<ApiResponseDTO<Void>>handleMemberNotFound(MemberNotFoundException e) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponseDTO.of(e.getMessage()));
+  }
+
+  @ExceptionHandler(InboundException.class)
+  public ResponseEntity<ApiResponseDTO<Void>> handleInboundException(InboundException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponseDTO.of(e.getMessage()));
+  }
+  @ExceptionHandler(InboundNotFoundException.class)
+  public ResponseEntity<ApiResponseDTO<Void>> handleInboundNotFoundException(InboundNotFoundException e) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponseDTO.of(e.getMessage()));
   }
 }
