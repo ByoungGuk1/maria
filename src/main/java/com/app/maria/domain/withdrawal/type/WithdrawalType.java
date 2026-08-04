359e84ea541049b342fed8caea5d8aaaa50ebe21
gukhee <gukhee0117@gmail.com>
2026-08-04 10:42:44 +0900

    fix : 인출 type 수정

--- src/main/java/com/app/maria/domain/withdrawal/type/WithdrawalType.java
+++ src/main/java/com/app/maria/domain/withdrawal/type/WithdrawalType.java
@@ -1,6 +1,7 @@
 package com.app.maria.domain.withdrawal.type;
 
 public enum WithdrawalType {
-  EARNINGS,
-  PRINCIPAL
+  EARNINGS_ONLY,
+  MATURED_PRINCIPAL_INCLUDED,
+  IMMATURE_PRINCIPAL_INCLUDED
 }
