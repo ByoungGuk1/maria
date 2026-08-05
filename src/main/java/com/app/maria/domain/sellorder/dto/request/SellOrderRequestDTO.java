e3af3ac493cb591a8af1df10e6473fb699a043f6
ychun4012 <ychun4012@gmail.com>
2026-08-05 21:20:45 +0900

    style: SellOrderRequestDTO, JwtAuthenticationFilter 불필요한 주석 제거
    - 코드 변경 없이 가독성을 저해하는 주석만 정리

--- src/main/java/com/app/maria/domain/sellorder/dto/request/SellOrderRequestDTO.java
+++ src/main/java/com/app/maria/domain/sellorder/dto/request/SellOrderRequestDTO.java
@@ -21,7 +21,6 @@ public class SellOrderRequestDTO {
     @DecimalMin(value = "0", inclusive = false, message = "매도 수량은 0보다 커야 합니다.")
     private BigDecimal sellQty;
 
-    // inbound_detail DTO 연동 전 임시 필드
     @NotNull(message = "거래소 코드는 필수입니다.")
     private String exchangeCode;
 
