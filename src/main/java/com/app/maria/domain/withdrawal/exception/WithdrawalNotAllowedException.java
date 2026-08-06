fc37b88347a885d80195697f19b335e139767917
김민석 <105479136+kminseok-dev@users.noreply.github.com>
2026-08-06 22:34:20 +0900

    Revert "Feature/27"

--- src/main/java/com/app/maria/domain/withdrawal/exception/WithdrawalNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.app.maria.domain.withdrawal.exception;
-
-public class WithdrawalNotAllowedException extends RuntimeException {
-    public WithdrawalNotAllowedException(String message) {
-        super(message);
-    }
-}
