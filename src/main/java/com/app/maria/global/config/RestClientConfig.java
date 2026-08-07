946fb4b69f1e289b8319abc9620ff08db0bbaac1
iingjeong <silvmin42@gmail.com>
2026-08-07 16:18:54 +0900

    feat: mydata 연동용 RestClient 설정 추가

--- src/main/java/com/app/maria/global/config/RestClientConfig.java
+++ src/main/java/com/app/maria/global/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.app.maria.global.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,9 +9,18 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
+    @Qualifier("registrableStockRestClient")
     public RestClient restClient() {
         return RestClient.builder()
                 .baseUrl("http://localhost:10001")
+                .build();
+    }
+
+    @Bean
+    @Qualifier("mydataRestClient")
+    public RestClient mydataRestClient() {
+        return RestClient.builder()
+                .baseUrl("http://localhost:10002")
                 .build();
     }
 }
