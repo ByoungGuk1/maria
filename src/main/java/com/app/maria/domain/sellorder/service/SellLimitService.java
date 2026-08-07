93e9abf030283b1680211cbf2bb819ae2235305a
ychun4012 <ychun4012@gmail.com>
2026-08-07 16:19:21 +0900

    style: SellLimitService, SellOrderService 인터페이스 메서드 선언에서 불필요한 public 제거
    - 다른 인터페이스(AdminMapper, SellOrderMapper 등)와 스타일 통일
    - 인터페이스 메서드는 기본적으로 public abstract이므로 동작 변화 없음

--- src/main/java/com/app/maria/domain/sellorder/service/SellLimitService.java
+++ src/main/java/com/app/maria/domain/sellorder/service/SellLimitService.java
@@ -4,6 +4,6 @@ import java.math.BigDecimal;
 
 public interface SellLimitService {
 
-    public boolean isWithinSellLimit(Long inboundId, BigDecimal orderAmount);
+    boolean isWithinSellLimit(Long inboundId, BigDecimal orderAmount);
 
 }
