f3bdb2772ccc36396da95e76d84f1973146121de
iingjeong <silvmin42@gmail.com>
2026-08-11 14:24:37 +0900

    chore: apply spotlessApply formatting (inbound test)

--- src/test/java/com/app/maria/domain/inbound/service/InboundServiceImplTest.java
+++ src/test/java/com/app/maria/domain/inbound/service/InboundServiceImplTest.java
@@ -3,7 +3,6 @@ package com.app.maria.domain.inbound.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
